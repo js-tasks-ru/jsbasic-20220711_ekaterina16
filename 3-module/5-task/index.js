ef51e74d383e2d74e7d9c1a92c71ca97638e8688
kate2110 <kate2110@yandex-team.ru>
2022-07-23 14:09:49 +0300

    Переписала функцию на стрелочную

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,8 +1,7 @@
 function getMinMax(str) {
-  let arr = str.split(' ').filter((c) => !isNaN(parseInt(c))).map(function(item) {
-    return Number(item);
-  });
+  let arr = str.split(' ').filter((c) => !isNaN(parseInt(c))).map(item => Number(item));
 
+  // Spread (...) — принимает массив и расширяет его на отдельные элементы
   return {
     max: Math.max(...arr),
     min: Math.min(...arr),

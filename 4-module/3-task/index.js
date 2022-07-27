3d8075e748c2c72c5a962eaea1c2173b1d6aa9ec
kate2110 <kate2110@yandex-team.ru>
2022-07-27 13:55:48 +0300

    Домашка 4

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,44 @@
 function highlight(table) {
-  // ваш код...
+  /**
+   * Проставить класс available/unavailable в зависимости от значения атрибута data-available у ячейки Status.
+   * Если её значение true – класс available, если её значение false – класс unavailable.
+   */
+  let availableCells = table.querySelectorAll('td');
+
+  availableCells.forEach(cell => {
+    let row = cell.closest('tr');
+
+    if (cell.dataset.available === 'true') {
+      row.classList.add('available');
+    } else if (cell.dataset.available === 'false') {
+      row.classList.add('unavailable');
+    } else {
+      row.hidden = true;
+    }
+  });
+
+  // Добавить inline-стиль style="text-decoration: line-through", если значение ячейки Age меньше 18.
+  let ageSells = table.querySelectorAll('td');
+
+  ageSells.forEach(cell => {
+    let row = cell.closest('tr');
+
+    if (cell.textContent < 18) {
+      row.style.textDecoration = "line-through";
+    }
+  });
+
+  //Проставить класс male/female в зависимости от содержимого ячейки Gender.
+  // Если её значение m – класс male, Если её значение f – класс female.
+  let genderSells = table.querySelectorAll('td');
+
+  genderSells.forEach(cell => {
+    let row = cell.closest('tr');
+
+    if (cell.textContent === "m") {
+      row.classList.add("male");
+    } else {
+      row.classList.add("female");
+    }
+  });
 }

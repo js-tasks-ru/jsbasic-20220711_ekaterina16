79652ba25e9ae4ee0974985ee9507598bcd92acd
kate2110 <kate2110@yandex-team.ru>
2022-07-27 14:53:36 +0300

    Правки по ревью

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,11 +1,11 @@
 function highlight(table) {
+  let cells = table.querySelectorAll('td');
+
   /**
    * Проставить класс available/unavailable в зависимости от значения атрибута data-available у ячейки Status.
    * Если её значение true – класс available, если её значение false – класс unavailable.
    */
-  let availableCells = table.querySelectorAll('td');
-
-  availableCells.forEach(cell => {
+  cells.forEach(cell => {
     let row = cell.closest('tr');
 
     if (cell.dataset.available === 'true') {
@@ -18,9 +18,7 @@ function highlight(table) {
   });
 
   // Добавить inline-стиль style="text-decoration: line-through", если значение ячейки Age меньше 18.
-  let ageSells = table.querySelectorAll('td');
-
-  ageSells.forEach(cell => {
+  cells.forEach(cell => {
     let row = cell.closest('tr');
 
     if (cell.textContent < 18) {
@@ -30,9 +28,7 @@ function highlight(table) {
 
   //Проставить класс male/female в зависимости от содержимого ячейки Gender.
   // Если её значение m – класс male, Если её значение f – класс female.
-  let genderSells = table.querySelectorAll('td');
-
-  genderSells.forEach(cell => {
+  cells.forEach(cell => {
     let row = cell.closest('tr');
 
     if (cell.textContent === "m") {

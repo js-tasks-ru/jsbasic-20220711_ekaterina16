b58639bc6a31aadefccc54e28eaa7f7ae66d6c17
kate2110 <kate2110@yandex-team.ru>
2022-07-30 22:45:48 +0300

    Домашка 5 - карусель

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,36 @@
 function initCarousel() {
-  // ваш код...
+  let buttonRight = document.querySelector('[class="carousel__arrow carousel__arrow_right"]');
+  let buttonLeft = document.querySelector('[class="carousel__arrow carousel__arrow_left"]');
+  let images = document.querySelectorAll(`[class="carousel__slide"]`);
+  let image = document.querySelector(`[class="carousel__inner"]`);
+
+  let sliderIndex = 0;
+  let maxIndex = images.length - 1;
+  buttonLeft.style.display = 'none';
+
+  buttonRight.onclick = () => {
+    if (sliderIndex !== maxIndex) {
+      sliderIndex++;
+    }
+
+    buttonLeft.style.display = '';
+
+    if (sliderIndex === maxIndex) {
+      buttonRight.style.display = 'none';
+    }
+
+    image.style.transform = `translateX(${sliderIndex * -100}%)`;
+  };
+
+  buttonLeft.onclick = () => {
+    if (sliderIndex > 0) {
+      sliderIndex--;
+    }
+
+    if (sliderIndex === 0) {
+      buttonLeft.style.display = 'none';
+    }
+
+    image.style.transform = `translateX(${sliderIndex * 100}%)`;
+  };
 }

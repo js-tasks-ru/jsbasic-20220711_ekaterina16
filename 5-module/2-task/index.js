aa827424e56f07dd29927af258698caead472ba9
kate2110 <kate2110@yandex-team.ru>
2022-07-30 17:11:34 +0300

    Домашка 5 (первые 2 задачи)

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,8 @@
 function toggleText() {
-  // ваш код...
+  let button = document.querySelector('[class="toggle-text-button"]');
+  let text = document.querySelector('[id="text"]');
+
+  button.onclick = () => {
+    text.hidden = text.hidden === false;
+  };
 }

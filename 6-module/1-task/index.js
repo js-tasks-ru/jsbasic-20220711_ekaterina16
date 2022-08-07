02dd566f5ec169f6438feb0a0c566d47361b4a5c
kate2110 <kate2110@yandex-team.ru>
2022-08-07 13:41:27 +0300

    Домашка 6

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -13,6 +13,49 @@
  *
  */
 export default class UserTable {
+  elem = '';
+
   constructor(rows) {
+    let tbody = document.createElement('tbody');
+    let table = document.createElement('table');
+
+    for (let item of rows) {
+      let tr = document.createElement('tr');
+
+      tr.innerHTML = `
+        <td>${item.name}</td>
+        <td>${item.age}</td>
+        <td>${item.salary}</td>
+        <td>${item.city}</td>`;
+      tbody.appendChild(tr);
+
+      table.appendChild(tbody);
+
+      tr.classList.add("row");
+      table.classList.add("myTable");
+    }
+
+    this.elem = table;
+
+    let row = table.querySelectorAll("tbody tr");
+
+    for (let i = 0; i < row.length; i++) {
+      row[i].innerHTML += '<td><button class="remove-button" data-action="remove">X</button></td>';
+    }
+
+    table.querySelector("tbody").addEventListener("click", function(e) {
+      e.target.closest('.row').remove();
+    });
   }
 }
+
+
+
+
+
+
+
+
+
+
+
